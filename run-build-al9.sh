9e1a78dca6302f991a9540225ac503bcbf1cb1f9
Kenneth Davidson <xylene2016@gmail.com>
2026-02-22 17:58:39 -0500

    Create run-build-al9.sh

---
+++ run-build-al9.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+TZ='UTC'; export TZ
+umask 022
+set -e
+cd "$(dirname "$0")"
+systemctl start docker
+sleep 5
+echo
+cat /proc/cpuinfo
+echo
+if [ "$(cat /proc/cpuinfo | grep -i '^processor' | wc -l)" -gt 1 ]; then
+    #docker run --cpus="$(cat /proc/cpuinfo | grep -i '^processor' | wc -l).0" --rm --name al9 -itd almalinux:9 bash
+    docker run --cpus="$(cat /proc/cpuinfo | grep -i '^processor' | wc -l).0" --rm --name al9 -itd quay.io/almalinuxorg/almalinux:9 bash
+else
+    #docker run --rm --name al9 -itd almalinux:9 bash
+    docker run --rm --name al9 -itd quay.io/almalinuxorg/almalinux:9 bash
+fi
+sleep 2
+docker exec al9 dnf clean all
+docker exec al9 dnf makecache
+docker exec al9 dnf install -y wget bash gcc g++ cmake m4 pkgconf clang llvm glibc-devel
+docker exec al9 /bin/bash -c 'ln -svf bash /bin/sh'
+docker exec al9 dnf update -y
+docker exec al9 /bin/bash -c 'rm -fr /tmp/*'
+docker cp al9 al9:/home/
+docker exec al9 /bin/bash /home/al9/brg.sh
+mkdir -p /tmp/_output_assets
+docker cp al9:/tmp/_output /tmp/_output_assets/
+
+exit
